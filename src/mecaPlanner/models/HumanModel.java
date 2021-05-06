692c8ea9df6bc27e8dd10ef32d283c1477c159a4
dbuckingham <david.buckingham@tufts.edu>
2021-05-05 20:42:05 -0400

    finnished example depl

--- src/mecaPlanner/models/HumanModel.java
+++ src/mecaPlanner/models/HumanModel.java
@@ -28,45 +28,44 @@ public class HumanModel extends Model {
         Set<Action> prediction = new HashSet<>();
 
 
-        for (Action a : allActions) {
-            if (a.getName().equals("wait")) {
-                prediction.add(a);
-                return prediction;
-            }
-        }
-        throw new RuntimeException("Human model failed with wait");
-
-//        for (Action a : allActions) {
-//            if (a.getName().equals("eat")) {
-//                prediction.add(a);
-//                return prediction;
-//            }
-//        }
-//
-//        if (ndState.necessarily(new Fluent("at", "pizza", "roomB"))) {
-//            if (ndState.necessarily(new Fluent("at", "human1", "roomA"))) {
-//                prediction.add(getSafeActionBySignature("move(human1,roomA, hall1)", ndState));
-//                prediction.add(getSafeActionBySignature("move(human1,roomA, hall2)", ndState));
-//                return prediction;
-//            }
-//            else if (ndState.necessarily(new Fluent("at", "human1", "hall1"))) {
-//                prediction.add(getSafeActionBySignature("move(human1,hall1, roomB)", ndState));
-//                return prediction;
-//            }
-//            else if (ndState.necessarily(new Fluent("at", "human1", "hall2"))) {
-//                prediction.add(getSafeActionBySignature("move(human1,hall2, roomB)", ndState));
-//                return prediction;
-//            }
-//            throw new RuntimeException("Human model failed with pizza");
-//        }
-//
 //        for (Action a : allActions) {
 //            if (a.getName().equals("wait")) {
 //                prediction.add(a);
 //                return prediction;
 //            }
 //        }
-//        throw new RuntimeException("Human model failed");
+
+        for (Action a : allActions) {
+            if (a.getName().equals("eat")) {
+                prediction.add(a);
+                return prediction;
+            }
+        }
+
+        if (ndState.necessarily(new Fluent("at", "pizza", "roomB"))) {
+            if (ndState.necessarily(new Fluent("at", "human1", "roomA"))) {
+                prediction.add(getSafeActionBySignature("move(human1,roomA, hall1)", ndState));
+                prediction.add(getSafeActionBySignature("move(human1,roomA, hall2)", ndState));
+                return prediction;
+            }
+            else if (ndState.necessarily(new Fluent("at", "human1", "hall1"))) {
+                prediction.add(getSafeActionBySignature("move(human1,hall1, roomB)", ndState));
+                return prediction;
+            }
+            else if (ndState.necessarily(new Fluent("at", "human1", "hall2"))) {
+                prediction.add(getSafeActionBySignature("move(human1,hall2, roomB)", ndState));
+                return prediction;
+            }
+            throw new RuntimeException("Human model failed with pizza");
+        }
+
+        for (Action a : allActions) {
+            if (a.getName().equals("wait")) {
+                prediction.add(a);
+                return prediction;
+            }
+        }
+        throw new RuntimeException("Human model failed");
 
 
     }
